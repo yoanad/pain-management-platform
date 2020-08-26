7454b4a55fcff68efe0b46045334fa412e2cde40
yoanad <yoana.dimova@gmail.com>
2020-08-26 13:52:50 +0200

    Add filters

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,9 @@
 import React, { useState, useCallback } from 'react';
-import { Button, TextField, CircularProgress, Typography } from 'rmwc';
+import {
+    Button, TextField, CircularProgress, Typography, Drawer, DrawerHeader,
+    DrawerTitle, DrawerSubtitle, DrawerContent, ListItem, List, ChipSet, Switch,
+    Chip, Slider
+} from 'rmwc';
 import './Search.css'
 import Card from '../Card';
 import doctors from '../../services/doctors';
@@ -11,6 +15,13 @@ const Search = () => {
     const [loading, setLoading] = useState(false);
     const [filteredItems, setFilteredItems] = useState([]);
     const [searched, setSearched] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = React.useState({});
+    const toggleSelected = key =>
+        setSelected({
+            ...selected,
+            [key]: !selected[key]
+        });
 
     const handleClick = useCallback(() => {
         setFilteredItems(
@@ -19,10 +30,10 @@ const Search = () => {
                 && doctor.location.toLowerCase().includes(locationFilter.toLowerCase())
             )
         );
-        // (specialtyFilter.toLowerCase().length !== 0 && locationFilter.toLowerCase().length !== 0)
         setSearched(true);
 
     }, [specialtyFilter, locationFilter]);
+
 
     const NoResultsInitial = () => {
         return (
@@ -80,9 +91,70 @@ const Search = () => {
                 <Button icon="search" label="Search" raised onClick={handleClick} />
                 <div className="Background"></div>
             </div>
+            <div className="Filters-Button">
+                <Button label="more filters" icon="add" onClick={() => setOpen(!open)} />
+            </div>
             <div className="Results">
                 <Results />
             </div>
+            <Drawer className="Drawer" modal open={open} onClose={() => setOpen(false)}>
+                <DrawerHeader>
+                    <DrawerTitle>More filters</DrawerTitle>
+                </DrawerHeader>
+                <DrawerContent>
+                    <h2>Specialty</h2>
+                    <div className="Specialty">
+                        <ChipSet filter>
+                            <Chip
+                                selected={selected.physiotherapy}
+                                checkmark
+                                onInteraction={(e) => toggleSelected('physiotherapy')}
+                                label="Physiotherapy"
+                            />
+                            <Chip
+                                selected={selected.massage}
+                                checkmark
+                                onInteraction={() => toggleSelected('massage')}
+                                label="Massage"
+                            />
+                            <Chip
+                                selected={selected.chiropractic}
+                                checkmark
+                                onInteraction={() => toggleSelected('chiropractic')}
+                                label="Chiropractic"
+                            />
+                            <Chip
+                                selected={selected.lbp}
+                                checkmark
+                                onInteraction={() => toggleSelected('lbp')}
+                                label="Lower back pain"
+                            />
+                            <Chip
+                                selected={selected.musclePain}
+                                checkmark
+                                onInteraction={() => toggleSelected('musclePain')}
+                                label="Muscle pain"
+                            />
+                            <Chip
+                                selected={selected.migraine}
+                                checkmark
+                                onInteraction={() => toggleSelected('migraine')}
+                                label="Migraine"
+                            />
+                        </ChipSet>
+                        <h2>Health insurance</h2>
+                        <Switch defaultChecked label="NHS" />
+                        <Switch defaultChecked label="Private" />
+                        <h2>Online booking</h2>
+                        <Switch defaultChecked label="With online booking" />
+                        <h2>Video consultation</h2>
+                        <Switch defaultChecked label="With video consultation" />
+                        <h2>Experience (years)</h2>
+                        <Slider discrete displayMarkers min={1} max={20} step={1} />
+                    </div>
+                    <Button label="Done" raised onClick={() => setOpen(false)} />
+                </DrawerContent>
+            </Drawer>
         </div>
     )
 }
